3b286c71f58b2adcbe5404747b823ee42b78ec9f
Deyan Reza <rezadeyan@gmail.com>
2025-07-08 11:13:05 -0400

    Skeleton and input files for testing

---
+++ Verbage.py
@@ -0,0 +1,21 @@
+#Task: Take in a script with 6 people: M1,M2,M3,F1,F2,F3 and determine if Females use more verbs than Males on average and if it is statistically significant. (Given this is a script in which everyone has ~30+ lines)
+
+# Step 1: Import libraries
+
+# Step 2: Import dataset
+
+# Step 3: Input script
+
+# Step 4: Make 2 lists (male and female)
+
+# Step 5: Count verbs per line per person and add to corresponding list
+
+# Step 6: Find average for each group
+
+# Step 7: Find sample variances
+
+# Step 8: Degrees of Freedom
+
+# Step 9: Conduct Welchs t-test
+
+# Step 10: Output
